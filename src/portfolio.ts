2265924f0ebf67e41752e7c45a7ff27569800ce3
Søren Døssing <netcom@sauber.net>
2024-11-04 14:08:15 +0100

    Use reduce() for stats

--- src/portfolio.ts
+++ src/portfolio.ts
@@ -30,30 +30,26 @@ export class Portfolio {
 
   /** Total amount invested in positions */
   public get invested(): number {
-    // values.reduce((total: number, a: number) => total + a, 0);
-    let sum = 0;
-    for (const position of this.positions) {
-      sum += position.invested;
-    }
-    return sum;
+    return this.positions.reduce(
+      (sum: number, p: Position) => sum + p.invested,
+      0
+    );
   }
 
   /** Total unrealized profit of all positions */
   public profit(time: Date = new Date()): number {
-    let sum = 0;
-    for (const position of this.positions) {
-      sum += position.profit(time);
-    }
-    return sum;
+    return this.positions.reduce(
+      (sum: number, p: Position) => sum + p.profit(time),
+      0
+    );
   }
 
   /** Total unrealized value of all positions */
   public value(time: Date = new Date()): number {
-    let sum = 0;
-    for (const position of this.positions) {
-      sum += position.value(time);
-    }
-    return sum;
+    return this.positions.reduce(
+      (sum: number, p: Position) => sum + p.value(time),
+      0
+    );
   }
 
   /** Printable statement */
@@ -61,29 +57,39 @@ export class Portfolio {
     const money = (v: number): number => parseFloat(v.toFixed(2));
     const now: Date = new Date();
     const t = new Table();
-    // t.title = "Positions";
-    t.headers = ["Date", "Symbol", "Price", "Units", "Invested", "Profit", "Value"];
+    t.headers = [
+      "Date",
+      "Symbol",
+      "Price",
+      "Units",
+      "Invested",
+      "Profit",
+      "Value",
+    ];
     let invested = 0;
     let profit = 0;
     let value = 0;
-    t.rows = this.positions.map(p=>{
+    t.rows = this.positions.map((p) => {
       const positionValue = p.value(now);
 
-      // Summary
+      // Portfolio stats
       value += positionValue;
       invested += p.invested;
       profit += positionValue - p.invested;
 
       return [
-      now.toLocaleDateString(),
-      p.instrument.symbol,
-      money(p.price),
-      p.units.toFixed(4),
-      money(p.invested),
-      money(positionValue - p.invested),
-      money(positionValue),
-    ]});
-    t.title = `Portfolio position=${this.positions.length}, invested=${money(invested)}, profit=${money(profit)}, value=${money(value)}`;
+        now.toLocaleDateString(),
+        p.instrument.symbol,
+        money(p.price),
+        p.units.toFixed(4),
+        money(p.invested),
+        money(positionValue - p.invested),
+        money(positionValue),
+      ];
+    });
+    t.title = `Portfolio position=${this.positions.length}, invested=${money(
+      invested
+    )}, profit=${money(profit)}, value=${money(value)}`;
     return t.toString();
   }
 }
